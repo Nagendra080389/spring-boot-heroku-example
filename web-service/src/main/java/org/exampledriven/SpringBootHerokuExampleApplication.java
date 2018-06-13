cd82422c744974dfb52eb59cc723740568a9f69f
nagesingh@deloitte.com <nagesingh@deloitte.com>
2018-06-13 13:32:44 +0530

    Change listener

--- web-service/src/main/java/org/exampledriven/SpringBootHerokuExampleApplication.java
+++ web-service/src/main/java/org/exampledriven/SpringBootHerokuExampleApplication.java
@@ -1,6 +1,6 @@
 package org.exampledriven;
 
-import org.exampledriven.rabbitMQ.RabbitMQListener;
+
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
@@ -17,35 +17,7 @@ public class SpringBootHerokuExampleApplication {
 
     public final static String PDF_MERGE_QUEUE= "pdf-merge-queue";
 
-    @Bean
-    Queue queue(){
-        return new Queue(PDF_MERGE_QUEUE, false);
-    }
 
-    @Bean
-    TopicExchange exchange(){
-        return new TopicExchange("pdf-merge-exchange");
-    }
-
-    @Bean
-    Binding binding(Queue queue, TopicExchange topicExchange){
-        return BindingBuilder.bind(queue).to(topicExchange).with(PDF_MERGE_QUEUE);
-    }
-
-    @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-                                             MessageListenerAdapter listenerAdapter){
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(PDF_MERGE_QUEUE);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(RabbitMQListener rabbitMQListener){
-        return new MessageListenerAdapter(rabbitMQListener, "receiveMessage");
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHerokuExampleApplication.class, args);
--- worker-service/src/main/java/org/exampledriven/SpringBootHerokuExampleApplication.java
+++ worker-service/src/main/java/org/exampledriven/SpringBootHerokuExampleApplication.java
@@ -1,11 +1,51 @@
 package org.exampledriven;
 
+import org.exampledriven.rabbitMQ.RabbitMQListener;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootHerokuExampleApplication {
     public final static String PDF_MERGE_QUEUE= "pdf-merge-queue";
+
+    @Bean
+    Queue queue(){
+        return new Queue(PDF_MERGE_QUEUE, false);
+    }
+
+    @Bean
+    TopicExchange exchange(){
+        return new TopicExchange("pdf-merge-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange topicExchange){
+        return BindingBuilder.bind(queue).to(topicExchange).with(PDF_MERGE_QUEUE);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter){
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(PDF_MERGE_QUEUE);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(RabbitMQListener rabbitMQListener){
+        return new MessageListenerAdapter(rabbitMQListener, "receiveMessage");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHerokuExampleApplication.class, args);
     }
